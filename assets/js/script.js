70e37e964f9ab85c0a095782bb81f3bc9803bb1b
richosj <okm760596@gmail.com>
2024-07-15 17:42:14 +0900

    ㅣㅆ발.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,36 @@
 (function(){
-    console.log('this')
+    console.log('this');
 
     // header
     window.addEventListener('scroll', () => {
         const header = document.getElementById('header');
         window.scrollY > 100 ? header.classList.add('scrolled') : header.classList.remove('scrolled');
-
     });
 
-    document.addEventListener("DOMContentLoaded", function() {
+    const texts = document.querySelectorAll("svg.text__motion text");
+    let x = 10; // 초기 x 위치
 
-        // 텍스트
-        gsap.to(".movie__timeline-01", { opacity: 1, duration: 1 , y: 0,});
-        gsap.to(".movie__timeline-02", { opacity: 1, duration: 1, y: 0, delay: 0.5 ,});
-        gsap.to(".movie__timeline-03", { opacity: 1, duration: 1, y: 0, delay: 1 ,});
-        gsap.to(".movie__timeline-04", { opacity: 1, duration: 1, delay: 1.25 ,});
-
-        gsap.to(".obj", { opacity: 1, scale: 1 , duration: 2, delay: 1.5, ease: "elastic.out(1, 0.3)" });
-        // 물방울
-        gsap.to(".movie__timeline-05", { opacity: 1, scale: 1 , duration: 1.1, delay: 1.8, ease: "elastic.out(1, 0.3)" });
-        gsap.to(".movie__timeline-06", { opacity: 1, scale: 1 , duration: 1.1, delay: 2, ease: "elastic.out(1, 0.3)" });
-        gsap.to(".movie__timeline-07", { opacity: 1, scale: 1 , duration: 1.1, delay: 2.2, ease: "elastic.out(1, 0.3)" });
+    texts.forEach((text, index) => {
+        text.setAttribute("x", x);
+        const textWidth = text.getBBox().width;
+        x += textWidth; // 글자 간 간격을 추가
     });
 
+    document.addEventListener("DOMContentLoaded", () => {
+        document.body.classList.add('onload');
 
-   
-    
+        const element = document.querySelector('.warwrap');
+        
+        function toggleActiveClass() {
+            element.classList.remove('active');
+            setTimeout(() => {
+                element.classList.add('active');
+            }, 10);
+        }
 
+        setTimeout(() => {
+            toggleActiveClass();
+            setInterval(toggleActiveClass, 3000);
+        }, 4000);
+    });
 })();
